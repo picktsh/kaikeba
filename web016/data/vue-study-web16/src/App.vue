acb8a18f2748759a3fccada63b2028c72c68fee2
tsh <1134512154@qq.com>
2020-01-02 02:34:24 +0800

    day02,第一天的学习代码

---
+++ web016/data/vue-study-web16/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <communication></communication> -->
+    <!-- <slot-example></slot-example> -->
+    <form-example></form-example>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import communication from '@/components/communication';
+import SlotExample from '@/components/slots'
+import FormExample from '@/components/form'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    communication,
+    SlotExample,
+    FormExample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
