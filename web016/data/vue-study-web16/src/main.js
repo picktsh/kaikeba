c5b167618e4b3de0a2325e529cc941714daf734c
tsh <1134512154@qq.com>
2020-01-03 03:07:01 +0800

    day02,代码

--- web016/data/vue-study-web16/src/main.js
+++ web016/data/vue-study-web16/src/main.js
@@ -3,11 +3,23 @@ import App from './App.vue'
 import './plugins/element.js'
 import create from './utils/create'
 
+// import router from './router'
+import router from './krouter'
+
+// import store from './store'
+import store from './kstore'
+
 Vue.config.productionTip = false
 // 事件总线
 Vue.prototype.$bus = new Vue()
-Vue.prototype.$create = create
+// Vue.prototype.$create = create
+Vue.use(create)
 
+// 3.挂载router实例，why？
 new Vue({
-  render: h => h(App),
+  // Vue.prototype.$router = router
+  router,
+
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web016/data/vue-study-web16/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img src="@/assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <communication></communication> -->
+    <!-- <slot-example></slot-example> -->
+    <!-- <form-example></form-example> -->
+    <p @click="$store.commit('add')">counter:{{$store.state.counter}}</p>
+    <p @click="$store.dispatch('add')">async counter:{{$store.state.counter}}</p>
+    <!-- <p>double counter:{{$store.getters.doubleCounter}}</p> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+import communication from '@/components/communication';
+import SlotExample from '@/components/slots'
+import FormExample from '@/components/form'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    communication,
+    SlotExample,
+    FormExample
+  },
+  // created () {
+  //   this.$store.state = {};
+  // },
+}
+</script>
