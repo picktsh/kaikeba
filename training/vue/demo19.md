1c93cb65ba9f958369012f4bdc137d016e222122
tsh <1134512154@qq.com>
2020-02-08 22:32:40 +0800

    training19,什么是一个递归组件？

--- training/vue/demo19.md
+++ training/vue/demo19.md
@@ -2,3 +2,32 @@
 
 ` Author rise created on 2020/2/7 `
 
+概念:递归组件是可以在他们自己的模板中调用自身的;
+
+递归组件,一定要有一个结束条件,否则就会一直循环引用,最终出现错误;我们可以使用`v-if="false"`作为递归组件的结束条件;当遇到`v-if`为`false`时,组件将不会再进行渲染;
+
+既然要用递归组件,那么对我们的数据格式肯定是需要满足递归条件的,就像下边这样,这是一个树状结构的递归数据
+```javascript
+let list = [
+    {
+        name: 'web全栈工程师',
+        cList: [
+            {
+                name: 'vue', cList: [
+                    {name: 'template'}
+                ]
+            },
+            {name: 'react',},
+        ]
+    },
+    {name: 'web高级工程师',},
+    {
+        name: 'web初级工程师',
+        cList: [
+            {name: 'html'},
+            {name: 'css'},
+            {name: 'javascript'},
+        ]
+    },
+]
+```
---
+++ training/vue/demo19.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<ul>
+			<li v-for="(item, index) in list" :key="item.name">
+				<p>{{item.name}}</p>
+				<tree-menu :list="item.cList" />
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "treeMenu",
+		components: {
+			treeMenu
+		},
+		data: function () {
+			return {
+				list: [
+					{
+						name: 'web全栈工程师',
+						cList: [
+							{
+								name: 'vue', cList: [
+									{name: 'template'}
+								]
+							},
+							{name: 'react',},
+						]
+					},
+					{name: 'web高级工程师',},
+					{
+						name: 'web初级工程师',
+						cList: [
+							{name: 'html'},
+							{name: 'css'},
+							{name: 'javascript'},
+						]
+					},
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
