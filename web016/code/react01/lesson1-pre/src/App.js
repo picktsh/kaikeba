981439f381c86d1d184aaa3f71927467567d75a2
tsh <1134512154@qq.com>
2020-02-20 21:57:04 +0800

    react01 笔记

---
+++ web016/code/react01/lesson1-pre/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import Button from "antd/es/button";
+// import "antd/dist/antd.css";
+
+import {Button} from "antd";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      app
+      <Button type="primary">提交</Button>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ web016/code/react01/lesson1-pre/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "./HomePage.less";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="homePage">
+        <h3>HomePage</h3>
+      </div>
+    );
+  }
+}
