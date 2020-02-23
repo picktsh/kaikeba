769a021193bebb51ad076f6fde03e788c772084f
tsh <1134512154@qq.com>
2020-02-24 00:36:09 +0800

    react02 lesson2-homework 基础

---
+++ web016/code/react02/lesson2-homework/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import ReduxPage from './pages/ReduxPage.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h3>写点东西表示页面正常</h3>
+      {/* Redux学习 */}
+      {/*<ReduxPage/>*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ web016/code/react02/lesson2-homework/src/pages/ReduxPage.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import store from "../store/";
+
+export default class ReduxPage extends Component {
+  componentDidMount() {
+    // 订阅
+    store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
+  
+  add = () => {
+    // 派发操作
+    store.dispatch({type: "ADD"});
+  };
+  minus = () => {
+    store.dispatch({type: "MINUS"});
+  };
+  asyAdd = () => {
+    // 派发操作
+    store.dispatch(dispatch => {
+      setTimeout(() => {
+        dispatch({type: "ADD"});
+      }, 1000);
+    });
+  };
+  
+  render() {
+    console.log("store", store); //sy-log
+    return (
+      <div>
+        <h3>ReduxPage</h3>
+        {/* getState获取数据 */}
+        <p>{store.getState()}</p>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+        <button onClick={this.asyAdd}>asyAdd</button>
+      </div>
+    );
+  }
+}
