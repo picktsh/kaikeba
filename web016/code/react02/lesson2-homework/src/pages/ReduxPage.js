5e390ef08b7227ff73fea6093009553f4f2270f7
tsh <1134512154@qq.com>
2020-02-25 01:14:22 +0800

    react02 lesson2-homework2 输入框计算,combineReducers

--- web016/code/react02/lesson2-homework/src/pages/ReduxPage.js
+++ web016/code/react02/lesson2-homework/src/pages/ReduxPage.js
@@ -26,16 +26,47 @@ export default class ReduxPage extends Component {
     });
   };
   
+  // ========== 华丽的分割线 ==========
+  // input的 事件,每次变更都把最新的值更新到store
+  onChange = (e) => {
+    console.log(e.target.value);
+    store.dispatch({
+      type: "ONCHANGE",
+      payload: {
+        value: Number(e.target.value)
+      }
+    });
+  };
+  CONFIRM = (value) => {
+    console.log('--confirm', Number(value));
+    if (Number(value) === 0 && !isNaN(Number(value))) return console.log('拜托,0就不要＋啦,没意义');
+    store.dispatch({
+      type: 'ADD',
+      // 把 countReducer 中的 数据给传过去!  除了从这里传还能从别的什么地方能访问到呢???
+      payload: {
+        inputValue: Number(value)
+      }
+    })
+  };
+  
   render() {
-    console.log("store", store); //sy-log
+    console.log("store 实例", store); //sy-log
+    const state = store.getState();
+    console.log('最终 state', state);
     return (
       <div>
         <h3>ReduxPage</h3>
         {/* getState获取数据 */}
-        <p>state: {store.getState()}</p>
+        <p>state: {state.countReducer}</p>
         <button onClick={this.add}>add +1</button>
         <button onClick={this.minus}>minus -1</button>
         <button onClick={this.asyAdd}>asyAdd +1</button>
+        <hr/>
+        输入框的值每变化一次就会更新对应的store <br/>
+        <input type="number" value={state.inputReducer.inputValue} onChange={(e) => this.onChange(e)}/>
+        {state.inputReducer.inputValue}
+        <button onClick={() => this.CONFIRM(state.inputReducer.inputValue)}>+</button>
+        <br/>点击按钮,将这个值和最上面的数字进行 + 计算 <br/>
       </div>
     );
   }
--- web016/code/react02/lesson2-homework/src/store/index.js
+++ web016/code/react02/lesson2-homework/src/store/index.js
@@ -1,9 +1,19 @@
-// import {createStore, applyMiddleware} from 'redux'
-import {createStore, applyMiddleware} from '../lib/kRedux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
 
-function reducer(state = 0, action) {
+// import {createStore, applyMiddleware} from '../lib/kRedux'
+
+function countReducer(state = 1, action) {
+  
   switch (action.type) {
     case 'ADD':
+      // console.log(action);
+      let inputValue = action.payload && action.payload.inputValue;
+      if (inputValue) {
+        return state + inputValue;
+      } else if (inputValue === 0 && !isNaN(inputValue)) {
+        // 0 算是个什么操作?虽然外面有加判断,安全起见这里还是要加判断
+        return state
+      }
       return state + 1;
     case 'MINUS':
       return state - 1;
@@ -12,12 +22,28 @@ function reducer(state = 0, action) {
   }
 }
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+// 输入框组的reducer,输入,确认添加,确认减少
+function inputReducer(state = {inputValue: 2}, action) {
+  switch (action.type) {
+    case 'ONCHANGE':
+      return {inputValue: action.payload.value};
+    // case 'CONFIRM':
+    //   这里有办法拿到其他reducer的值吗?允许这样吗?似乎不合理也不可以
+    //   console.log('--CONFIRM', state);
+    //   return {...state, countReducer: action.payload.countReducer + state.inputValue};
+    // // return state;
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({countReducer, inputReducer});
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 export default store;
 
 function logger({getState, dispatch}) {
   return dispatch => action => {
-    console.log(action.type + " 执行了"); //sy-log
+    console.log(action.type + " 执行了;", 'payload:', action.payload); //sy-log
     return dispatch(action);
   };
 }
