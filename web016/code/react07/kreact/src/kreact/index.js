336e570f6cfccee9e8bc9fe6adb269688598b99d
tsh <1134512154@qq.com>
2020-03-09 23:08:34 +0800

    手写react的 createElement,render,Component

---
+++ web016/code/react07/kreact/src/kreact/index.js
@@ -0,0 +1,35 @@
+//!vnode 就代表虚拟dom节点
+//! node代表真实dom节点
+
+// 接收type, props, children， 返回一个vnode
+function createElement(type, props, ...children) {
+  // console.log("createElement", arguments); //sy-log
+  if (props) {
+    delete props.__source;
+    delete props.__self;
+  }
+
+  return {
+    type: type,
+    props: {
+      ...props,
+      //!这里的处理与源码稍有不同，源里的话，只有一个元素，children是对象，多于一个的时候，是数组
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextNode(child)
+      )
+    }
+  };
+}
+
+function createTextNode(text) {
+  return {
+    type: "TEXT",
+    props: {
+      children: [],
+      nodeValue: text
+    }
+  };
+}
+export default {
+  createElement
+};
