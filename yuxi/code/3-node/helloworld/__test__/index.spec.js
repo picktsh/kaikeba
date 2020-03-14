837a93f110461b1f91a8282f3e5c89bf66ce5a30
tsh <1134512154@qq.com>
2020-03-14 22:59:47 +0800

    node helloWorld/index.js test

--- yuxi/code/3-node/helloworld/__test__/index.spec.js
+++ yuxi/code/3-node/helloworld/__test__/index.spec.js
@@ -1,4 +1,6 @@
 test('测试hello World', () => {
   const helloKkb = require('../index')
-  console.log('helloKkb', helloKkb);
+  // console.log('helloKkb', helloKkb);
+  expect(helloKkb)
+    .toBe('hello kkb')
 })
