35dd12abc043ec3a75fe7c5f196d1b9e3eb44277
tsh <1134512154@qq.com>
2020-03-20 00:19:52 +0800

    node02 kkb.js

--- web016/code/node02/02a/source/kkb.js
+++ web016/code/node02/02a/source/kkb.js
@@ -1,15 +1,37 @@
 const http = require('http')
+const context = require('./context')
+const request = require('./request')
+const response = require('./response')
 
 class KKB {
   listen(...args) {
     const server = http.createServer((req, res) => {
-      this.callback(req, res)
+      // this.callback(req, res)
+      
+      // 创建上下文
+      const ctx = this.createContext(req, res)
+      this.callback(ctx)
+      // 响应
+      res.end(ctx.body)
     })
     server.listen(...args)
   }
   
   use(callback) {
     this.callback = callback
+  }
+  
+  /**
+   * 构建上下文
+   */
+  createContext(req, res) {
+    const ctx = Object.create(context)
+    ctx.request = Object.create(request)
+    ctx.response = Object.create(response)
+    
+    ctx.req = request.req = req
+    ctx.res = response.res = res
+    return ctx
   }
 }
 
