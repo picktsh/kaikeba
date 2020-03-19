ddd9f2713291725193fb82da3d81f4499d87e533
tsh <1134512154@qq.com>
2020-03-20 00:13:10 +0800

    node02 context.js,request.js,response.js

--- web016/code/node02/02a/source/app.js
+++ web016/code/node02/02a/source/app.js
@@ -1,10 +1,15 @@
 const KKB = require('./KkB')
 const app = new KKB()
 
-app.use((req, res) => {
-  res.writeHead(200)
-  res.end('hi kkb')
+// app.use((req, res) => {
+//   res.writeHead(200)
+//   res.end('hi kkb')
+// })
+
+app.use(ctx => {
+  ctx.body = 'hehe'
 })
+
 app.listen(3000, () => {
   console.log('listen at 3000');
 })
