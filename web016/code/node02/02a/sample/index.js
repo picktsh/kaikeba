88fd20589f90a005c9d11ce1897bbb781768aac7
tsh <1134512154@qq.com>
2020-03-19 23:30:37 +0800

    node02-sample koa

--- web016/code/node02/02a/sample/index.js
+++ web016/code/node02/02a/sample/index.js
@@ -1,8 +1,18 @@
 const Koa = require('koa');
 const app = new Koa();
-app.use((ctx, next) => {
+// 切面
+app.use(async (ctx, next) => {
+  const start = new Date().getTime();
+  console.log(`start: ${ctx.url}`);
+  await next()
+  const end = new Date().getTime();
+  console.log(`请求${ctx.url},耗时${end - start}ms`);
+});
+app.use(async (ctx, next) => {
   ctx.body = [
     {name: 'tom'}
-  ]
+  ];
+  await next()
 });
+
 app.listen(3000);
