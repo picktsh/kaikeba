8bb4b4de3416d44f72e8f460aaf3960fccc3fa0f
tsh <1134512154@qq.com>
2020-03-23 23:46:43 +0800

    node03 socketio 实现简易聊天室

--- web016/code/node03/socketio/index.js
+++ web016/code/node03/socketio/index.js
@@ -1,3 +1,27 @@
 // websocket的应用
 // 1.简易的聊天室
 // 2.浏览器控制台
+var app = require('express')();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connection');
+  
+  // 响应用户发送的信息
+  socket.on('chat message', function (msg) {
+    console.log(msg);
+    // 广播给所有人
+    io.emit('chat message', msg)
+  });
+  socket.on('disconnect', function () {
+    console.log('user disconnect');
+  })
+});
+http.listen(3001, function () {
+  console.log('listen on 3001');
+});
