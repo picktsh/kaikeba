28086a309bf84507d5d749d2219e83157214d530
tsh <1134512154@qq.com>
2020-03-25 02:52:57 +0800

    node03 更改端口尝试部署,更改IP策略

--- web016/code/node03/socketio/index.js
+++ web016/code/node03/socketio/index.js
@@ -1,26 +1,44 @@
-// websocket的应用
-// 1.简易的聊天室
-// 2.浏览器模仿控制台
+/**
+ * websocket的应用
+ * 1.简易的聊天室
+ * 2.浏览器模仿控制台
+ */
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 // const uuid = require('uuid');
 // console.log(uuid.v1());
+app.set('trust proxy', true);
+let userIP = '';
+
 // 请求根根路径
 app.get('/', function (req, res) {
+  userIP = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0];
+  res.sendFile(__dirname + '/index.html')
+});
+// 给前端一个IP的接口
+app.get('/ip', function (req, res) {
+  userIP = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0];
+  res.send(`const userIP = '${userIP}'`)
+});
+// 所有请求--暂时全部显示为index.html
+app.get('*', function (req, res) {
+  userIP = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0];
   res.sendFile(__dirname + '/index.html')
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connection');
+  console.log('user connection', userIP);
   // 响应用户发送的信息
   socket.on('chat message', function (msg) {
-    console.log(msg);
+    console.log('前', msg);
+    const msg2 = Object.assign({}, msg, {cip: userIP})
+    console.log('后', msg2);
     // 广播给所有人
-    io.emit('chat message', msg)
+    io.emit('chat message', msg2)
   });
   socket.on('disconnect', function () {
-    console.log('user disconnect');
+    console.log('user disconnect', userIP);
   })
 });
 http.listen(9000, function () {
