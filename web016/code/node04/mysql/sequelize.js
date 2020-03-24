2ad03c7b136a77cd237c8fd5afca2afe7291f0b7
tsh <1134512154@qq.com>
2020-03-24 21:57:33 +0800

    node03 sequelize.js

--- web016/code/node04/mysql/sequelize.js
+++ web016/code/node04/mysql/sequelize.js
@@ -12,9 +12,24 @@
     stock: {type: Sequelize.INTEGER, defaultValue: 0},
   })
   
-  let ret = await Fruit.sync()
+  // let ret = await Fruit.sync()
+  let ret = await Fruit.sync({force: true})
+  
+  // 加入一条数据
   ret = await Fruit.create({
     name: '香蕉',
     price: 3.5,
+  });
+  // 变更价格,更新操作
+  ret = await Fruit.update({
+    price: 4
+  }, {
+    where: {name: '香蕉'}
   })
-})()
+  // 查询
+  const Op = Sequelize.Op; // 操作符
+  ret = await Fruit.findAll({
+    where: {price: {[Op.lt]: 5, [Op.gt]: 2}}
+  })
+  console.log(JSON.stringify(ret))
+})();
