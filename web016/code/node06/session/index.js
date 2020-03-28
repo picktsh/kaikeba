a17844fbcd271d74182b801800063bf0420b72d7
tsh <1134512154@qq.com>
2020-03-28 23:46:29 +0800

    node06 session

--- web016/code/node06/session/index.js
+++ web016/code/node06/session/index.js
@@ -1,38 +1,60 @@
 const Koa = require('koa')
+const router = require('koa-router')()
+const session = require('koa-session')
+const cors = require('koa2-cors')
+const bodyParser = require('koa-bodyparser')
+const static = require('koa-static')
+const app = new Koa();
 
-const app = new Koa()
+//配置session的中间件
+app.use(cors({
+  credentials: true
+}))
+app.keys = ['some secret'];
 
-// 鉴权
+app.use(static(__dirname + '/'));
+app.use(bodyParser())
+app.use(session(app));
+
 app.use((ctx, next) => {
   if (ctx.url.indexOf('login') > -1) {
     next()
   } else {
+    console.log('session', ctx.session.userinfo)
     if (!ctx.session.userinfo) {
-      ctx.body('登陆失败')
+      ctx.body = {
+        message: "登录失败"
+      }
     } else {
       next()
     }
   }
-});
-router.post('/users/login', async ctx => {
-  const {body} = ctx.request
-  // 设置session
-  ctx.session.userinfo = body.username; // 这里简写了
+})
+
+router.post('/login', async (ctx) => {
+  const {
+    body
+  } = ctx.request
+  console.log('body', body)
+  //设置session
+  ctx.session.userinfo = body.username;
   ctx.body = {
-    message: '登陆成功'
+    message: "登录成功"
   }
-});
-router.post('/users/logout', async ctx => {
+})
+router.post('/logout', async (ctx) => {
+  //设置session
   delete ctx.session.userinfo
   ctx.body = {
-    message: '登出成功'
+    message: "登出系统"
   }
-});
-router.post('/users/getUser', async ctx => {
+})
+router.get('/getUser', async (ctx) => {
   ctx.body = {
-    message: '获取用户信息',
+    message: "获取数据成功",
     userinfo: ctx.session.userinfo
   }
-});
-app.listen(3000)
-
+})
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(3000);
