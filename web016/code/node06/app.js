3e8dfbcad365a75e2d1b4fd022b722ab826aa8d0
tsh <1134512154@qq.com>
2020-03-28 21:21:43 +0800

    node06 cookie koa koa-session koa-redis

--- web016/code/node06/app.js
+++ web016/code/node06/app.js
@@ -1,19 +1,35 @@
 const Koa = require('koa');
 const app = new Koa();
 const session = require('koa-session');
+const redisStore = require('koa-redis');
+const redis = require('redis');
+const redisClient = redis.createClient(6379, 'localhost');
+
 
 app.keys = ['some secret'];
 
-// 配置
-const SESS_CONFIG = {
+app.use(session({
   key: 'kkb:sess',
-  maxAge: 86400000,
-  httpOnly: true,
-  signed: true,
-};
+  store: redisStore({})
+}));
 
-// 注册
-app.use(session(SESS_CONFIG, app));
+// // 配置
+// const SESS_CONFIG = {
+//   key: 'kkb:sess',
+//   maxAge: 86400000,
+//   httpOnly: true,
+//   signed: true,
+// };
+//
+// // 注册
+// app.use(session(SESS_CONFIG, app));
+
+app.use(async (ctx, next) => {
+  const keys = await client.keys('*');
+  for (const key of keys) {
+    console.log(await client.get(key))
+  }
+})
 
 // 测试
 app.use(ctx => {
