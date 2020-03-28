4e6e18102df8556aa42596350643cd3909337dfd
tsh <1134512154@qq.com>
2020-03-28 23:24:58 +0800

    node06 redis.js

--- web016/code/node06/redis.js
+++ web016/code/node06/redis.js
@@ -2,3 +2,10 @@
  * 键值服务器
  * 早期出现,现在已经用的比较少了
  */
+const redis = require('redis')
+const client = redis.createClient(6379,'localhost')
+
+client.set('hello','hahaha')
+client.get('hello',function(err,v){
+  console.log('redis key:',v)
+})
