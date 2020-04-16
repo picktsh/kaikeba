34cdddb62bd581f6962aa8f232657190debb37dc
tsh <1134512154@qq.com>
2020-04-16 22:01:54 +0800

    project03 getFollowers 获取用户

--- web016/code/project03/wx/index.js
+++ web016/code/project03/wx/index.js
@@ -4,3 +4,19 @@ const conf = require('./conf')
 router.all('/wechat', wechat(conf).middleware(async (message, ctx) => {
   return 'Hello world!';
 }))
+// router.get('./getToken', async ctx => {
+//   const url = ` https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${conf.appid}&secret=${conf.appsecret}`
+//   ctx.body = 'getToken'
+// })
+// router.get('./getFollowers', async ctx => {
+//   const url = ` https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET`
+//   ctx.body = 'getFollowers'
+// })
+
+const WechatAPI = require('co-wechat-api')
+const api = new WechatAPI(conf.appid, conf.appsecret)
+router.get('./getFollowers', async ctx => {
+  let res = api.getFollowers()
+  res = await api.batchGetUsers(res.data.openid, 'zh_CN')
+  ctx.body = res
+})
