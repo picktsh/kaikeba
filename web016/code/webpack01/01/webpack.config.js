a204379cc1a5393406ef27871cb21cc19da20a40
tsh <1134512154@qq.com>
2020-05-07 22:12:05 +0800

    webpack01 构建模式: none production development

--- web016/code/webpack01/01/webpack.config.js
+++ web016/code/webpack01/01/webpack.config.js
@@ -6,12 +6,12 @@ module.exports = {
   // 上下文 项目打包的相对路径-正常情况不会动
   // context: process.cwd(),
   // 入口,执行构建的入口 项目入口 支持3种类型[字符串,数组,对象]
-  // entry: "./src/index.js",
+  entry: "./src/index.js",
   // entry: ["./src/index.js","./src/other.js"],
-  entry: {
-    main: "./src/index.js",
-    other: "./src/other.js",
-  },
+  // entry: {
+  //   main: "./src/index.js",
+  //   other: "./src/other.js",
+  // },
   // 出口
   output: {
     // 构建的文件资源放在哪?必须是绝对路径
@@ -25,5 +25,7 @@ module.exports = {
     // 只要组成entry的模块内容没有改动,则对应的hash不变
     // name
     // id
-  }
+  },
+  // 构建模式: none production development
+  mode: "development",
 }
