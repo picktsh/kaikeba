6f29d8da165699224ac64b45c1117df8c00ad1bf
tsh <1134512154@qq.com>
2020-05-07 21:19:52 +0800

    webpack01 01 webpack.config.js 默认配置

---
+++ web016/code/webpack01/01/src/index.js
@@ -0,0 +1,5 @@
+console.log("index.js");
+
+
+// webpack的默认配置
+
--- web016/code/webpack01/01/webpack.config.js
+++ web016/code/webpack01/01/webpack.config.js
@@ -1,9 +1,10 @@
-// webpack 时基于 nodejs的
+// webpack 是基于 nodejs的
+// webpack配置就是一个对象
 const path = require("path")
 
 module.exports = {
   // 入口,执行构建的入口 项目入口
-  entry: "scr/index.js",
+  entry: "./src/index.js",
   // 出口
   output: {
     // 构建的文件资源放在哪?必须是绝对路径
