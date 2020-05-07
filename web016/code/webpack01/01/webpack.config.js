ecb9bd03c973a76543a539bb91652336b98ed94d
tsh <1134512154@qq.com>
2020-05-07 21:05:28 +0800

    webpack01 01 webpack.config.js 默认配置

--- web016/code/webpack01/01/webpack.config.js
+++ web016/code/webpack01/01/webpack.config.js
@@ -1,8 +1,14 @@
 // webpack 时基于 nodejs的
+const path = require("path")
 
 module.exports = {
-  // 入口
-  entry: "",
+  // 入口,执行构建的入口 项目入口
+  entry: "scr/index.js",
   // 出口
-  output: {}
+  output: {
+    // 构建的文件资源放在哪?必须是绝对路径
+    path: path.resolve(__dirname, "./dist"),
+    // 构建的文件资源叫啥?
+    filename: "main.js"
+  }
 }
