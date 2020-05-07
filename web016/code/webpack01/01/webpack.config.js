d7e43b73cfd4d5a09f611a2ce680bd619a958fad
tsh <1134512154@qq.com>
2020-05-07 22:05:11 +0800

    webpack entry 对象方式 多入口多出口 hash

--- web016/code/webpack01/01/webpack.config.js
+++ web016/code/webpack01/01/webpack.config.js
@@ -19,9 +19,10 @@ module.exports = {
     // 构建的文件资源叫啥? 无论是多出口还是单出口,都推拿使用占位符
     // filename: "main.js",
     filename: "[name]-[hash:6].js",// 对象模式,多出口;占位符
-    // 占位符:
-    // hash
-    // chunkhash
+    // 占位符: 长度默认20,可以指定长度[hash:6]
+    // hash  // 整个相互的hash,每构建一次,就会有一个新的hash值
+    // chunkhash  // 根据不同入口entry进行依赖解析,构建对应的chunkhash,模块相应地hash
+    // 只要组成entry的模块内容没有改动,则对应的hash不变
     // name
     // id
   }
