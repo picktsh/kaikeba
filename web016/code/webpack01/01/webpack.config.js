098be21482a40cd8fe2e906bfe5a2304be7f878a
tsh <1134512154@qq.com>
2020-05-07 21:31:57 +0800

    webpack entry 对象方式 多入口-多页面应用

--- web016/code/webpack01/01/webpack.config.js
+++ web016/code/webpack01/01/webpack.config.js
@@ -5,13 +5,19 @@ const path = require("path")
 module.exports = {
   // 上下文 项目打包的相对路径-正常情况不会动
   // context: process.cwd(),
-  // 入口,执行构建的入口 项目入口
-  entry: "./src/index.js",
+  // 入口,执行构建的入口 项目入口 支持3种类型[字符串,数组,对象]
+  // entry: "./src/index.js",
+  // entry: ["./src/index.js","./src/other.js"],
+  entry: {
+    main: "./src/index.js",
+    other: "./src/other.js",
+  },
   // 出口
   output: {
     // 构建的文件资源放在哪?必须是绝对路径
     path: path.resolve(__dirname, "./dist"),
     // 构建的文件资源叫啥?
-    filename: "main.js"
+    // filename: "main.js",
+    filename: "[name].js",// 占位符
   }
 }
