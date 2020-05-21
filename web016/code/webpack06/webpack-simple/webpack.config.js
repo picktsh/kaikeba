00f08c9a57ffd363e5d9d53f16bd824ffe8f8fa8
rise <rise.666@qq.com>
2020-05-21 22:43:30 +0800

    webpack06 myLoaders

--- web016/code/webpack06/webpack-simple/webpack.config.js
+++ web016/code/webpack06/webpack-simple/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path")
 module.exports = {
     entry: "./src/index.js",
     mode: "development",
+    resolveLoader: ["node_modules", "./myLoaders"],
     output: {
         path: path.resolve(__dirname, "./src"),
         name: "[name].js"
