9cea111b832d5260522fdffe5b3e392c75073742
rise <rise.666@qq.com>
2020-06-05 21:42:27 +0800

    flutter http

--- web016/code/flutter01/01/flutterProject/lib/main.dart
+++ web016/code/flutter01/01/flutterProject/lib/main.dart
@@ -89,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$_counter略略略',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
