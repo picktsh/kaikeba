cc11f6e5f54bde8c074a506e043d8eb5ecec9bb2
rise <rise.666@qq.com>
2020-06-07 21:46:19 +0800

    flutter02 实战练习-手写音容一个组件

---
+++ web016/code/flutter02/flutterDemo/lib/Component/friends_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FriendListPage extends StatefulWidget {
+  FriendListPage({Key key}) : super(key: key);
+
+  @override
+  _FriendListPageState createState() => _FriendListPageState();
+}
+
+class _FriendListPageState extends State<FriendListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text("12sss3")));
+  }
+}
