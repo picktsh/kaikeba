76719a1f2bccff8b473b033a6c10ce22845b4eab
rise <rise.666@qq.com>
2020-06-30 23:50:25 +0800

    test 前端自动化测试 数字相加,字符和数字相加,字符相加

--- web016/code/test/kkb.js
+++ web016/code/test/kkb.js
@@ -2,5 +2,9 @@
 // 测试就是给定的输入,判断期望的输出
 // 这么测试这个代码呢?
 function add(a, b) {
-    return Number(a) + Number(b)
+    // 此处会有 "a"字符串和数字的值比较,所以使用 ==
+    if (Number(a) == a && Number(b) == b) {
+        return Number(a) + Number(b)
+    }
+    return a + b
 }
--- web016/code/test/kkb.test.js
+++ web016/code/test/kkb.test.js
@@ -23,14 +23,22 @@ function expect(ret) {
     }
 }
 
+// 测试代码 帮你整理逻辑,可以随时执行,
+// 1. git pre commit 执行
+// 2. 云平台每次push会执行
 test('测试数字相加', () => {
     expect(add(1, 2)).toBe(3)
 })
 
 
 // 新的开发模式 TDD
-// 比如说想让add函数支持字符串相加, TDD就是先写测试
-// 然后报错,再去写逻辑
-test('测试字符串相加', () => {
-    expect(add(1, 2)).toBe(3)
+// 比如说想让add函数支持字符串相加,
+// TDD就是先写测试,然后报错,再去写逻辑
+test('字符串和数字相加', () => {
+    expect(add(1, "2")).toBe(3)
+})
+
+
+test('字符串和字符串相加', () => {
+    expect(add("a", "b")).toBe("ab")
 })
