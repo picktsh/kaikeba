2c7d45bec6a2a7b560a13be0a74082c554fc91ec
rise <rise.666@qq.com>
2020-07-28 00:02:00 +0800

    预习 2-react基础预习课程,创建完页面-下载图标

--- yuxi/code/2-react基础预习课程/1-初步创建页面/my-app/src/components/BottomNav/index.js
+++ yuxi/code/2-react基础预习课程/1-初步创建页面/my-app/src/components/BottomNav/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import './index.less'
+
+const menu = [
+  { title: '首页', icon: 'home', link: '/', },
+  { title: '购物车', icon: 'home', link: '/cart', },
+  { title: '订单列表', icon: 'home', link: '/orderlist', },
+  { title: '用户中心', icon: 'home', link: '/user', },
+]
 
 export default class BottomNav extends Component {
   render() {
     return (
-      <div>
-        <h3>
-          BottomNav
-        </h3>
+      <div className="bottom-nav">
+        {menu.map((item, index) => {
+          return (
+            <MenuItem {...item} key={index}/>
+          )
+        })}
       </div>
     )
   }
+}
+
+function MenuItem({ title, icon }) {
+  return (
+    <div className="menu-item">
+      <span className={'iconfont icon-' + icon}/>
+      <span className="title">{title}</span>
+    </div>
+  )
+
 }
