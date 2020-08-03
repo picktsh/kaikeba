d83c65dc347f7813cbd22da16c717572cc433956
rise <rise.666@qq.com>
2020-08-03 22:51:38 +0800

    web项目训练营: 第二章第4节： 09-登录验证码功能

---
+++ training-web-project/front/layouts/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>导航</div>
+    <nuxt></nuxt>
+    <div>底部信息</div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "login"
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ training-web-project/front/pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
