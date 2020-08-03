13441212d78bb1e47b91311956477ef384e5c91d
rise <rise.666@qq.com>
2020-08-04 00:06:43 +0800

    web项目训练营: 第二章第4节： 09-登录验证码功能

--- training-web-project/front/pages/login.vue
+++ training-web-project/front/pages/login.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-
+    <el-form-item props="email">
+      <span><i class="el-icon-mobile"></i></span>
+      <el-input placeholder="用户名"></el-input>
+    </el-form-item>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  layout: 'login'
 }
 </script>
 
